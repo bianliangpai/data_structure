01545501e7a11cc3e9863a7d966b12898afa5d9d
bianliangpai <bianliangpai@gmail.com>
2021-08-28 10:38:31 +0800

    [ENH]: better print for BinaryHeap

--- test/heap/test_binary_heap.cc
+++ test/heap/test_binary_heap.cc
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "Heap/BinaryHeap.h"
 
 int main() {
@@ -5,10 +7,13 @@ int main() {
   for (uint32_t i{0}; i < 10; ++i) {
     bh.Push(10 - i);
   }
+  std::cout << "build tree:\n";
   bh.PrintDebugMsg();
   bh.Pop();
+  std::cout << "first pop:\n";
   bh.PrintDebugMsg();
   bh.Pop();
+  std::cout << "second pop:\n";
   bh.PrintDebugMsg();
 
   return 0;
